c7f4e3f160f379ad46cf4d56136bb81b378ea553
Beth Scaer <bscaer@gmail.com>
2021-01-26 15:01:35 -0500

    First checkin

---
+++ index.js
@@ -0,0 +1,11 @@
+// This is a simple four-page website about fetal development. 
+
+// The home page, index.html, displays a table listing details of the the stages of fetal development - Zygote/Blastocyst, Embryo, and Fetus. 
+// It links to the three other pages for 0-6 weeks, 6-12 weeks, and 12-40 weeks of fetal development 
+// It prompts the user to enter their baby's name and provides a "Save" button.
+//
+// The 0-6 Weeks page, 0-6weeks.html, displays a graphic showing prenatal development from 0 to 6 weeks and has links to the other three pages.
+//
+// The 6-12 Weeks page, 6-12weeks.html, displays a graphic showing prenatal development from 6 to 12 weeks and has links to the other three pages.
+//
+// The 12-40 Weeks page, 12-40weeks.html, displays a graphic showing prenatal development from 12 to 40 weeks and has links to the other three pages.
